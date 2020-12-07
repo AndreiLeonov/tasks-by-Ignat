ba570665392f4fa7d445873dff6ec1dbaacb94b3
AndreiLeonov <andrei_leonov93@mail.ru>
2020-12-07 23:35:13 +0300

    #13 done

--- src/p2-homeworks/h13/Requests.tsx
+++ src/p2-homeworks/h13/Requests.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import SuperCheckbox from "../h4/common/c3-SuperCheckbox/SuperCheckbox";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
-import { RequestsAPI } from "./RequestsAPI";
+import {RequestsAPI} from "./RequestsAPI";
 
 export function Requests() {
 
+    const [res, setRes] = useState(null);
+
     const onClickHandler = () => {
-        RequestsAPI.sendCheckboxValue(true).then( res => {
-            console.log(res)
-        })
+        RequestsAPI.sendCheckboxValue(true)
+            .then(res => {
+                console.log(res);
+                setRes(res);
+            })
 
     }
 
@@ -16,6 +20,7 @@ export function Requests() {
         <div>
             <SuperCheckbox checked={true}/>
             <SuperButton onClick={onClickHandler}>SEND REQUEST</SuperButton>
+            <div>{res ? JSON.stringify(res) : "на данный момент нет ответа от сервера"}</div>
         </div>
     );
 }
--- src/p2-homeworks/h13/RequestsAPI.tsx
+++ src/p2-homeworks/h13/RequestsAPI.tsx
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 export const RequestsAPI = {
     sendCheckboxValue(data: boolean) {
-        return axios.post ('https://neko-cafe-back.herokuapp.com/auth/test',{success: data})
+        return axios.post('https://neko-cafe-back.herokuapp.com/auth/test', {success: data})
+            .then(res => res.data.info)
+            .catch(err => err.response.data.errorText)
     }
 }
 
